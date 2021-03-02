c54502afaf542ba8262b8672594fd40c2ceba2eb
zomars <zomars@me.com>
2021-03-02 13:07:17 -0700

    Add dpi config

--- rw_visual.py
+++ rw_visual.py
@@ -10,7 +10,7 @@ while True:
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots(figsize=(15, 9))
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                edgecolor='none', s=1)
