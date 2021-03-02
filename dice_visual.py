e9b49fa80bc6b3a52495adef2d98f81cc41bccd6
zomars <zomars@me.com>
2021-03-02 13:44:04 -0700

    Rolling Dice of Different Sizes

--- dice_visual.py
+++ dice_visual.py
@@ -4,11 +4,11 @@ from die import Die
 
 # Create two D6 dice.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -25,6 +25,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 1000 times',
+my_layout = Layout(title='Results of rolling a D6 and D10 50000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
