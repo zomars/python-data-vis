ba664dc0864ec8f1e9997555038b637c11f63d66
zomars <zomars@me.com>
2021-03-02 13:22:16 -0700

    Refactoring

--- random_walk.py
+++ random_walk.py
@@ -12,6 +12,12 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """Decide which direction to go and how far to fo in that direction"""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
+
     def fill_walk(self):
         """Calculate all the points in the walk."""
 
@@ -19,13 +25,8 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go and how fat to go in that direction.
-            x_direction = choice([1])
-            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Reject moves that go nowhere
             if x_step == 0 and y_step == 0:
