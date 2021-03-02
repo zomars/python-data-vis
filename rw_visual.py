935eac109ffff30f1bc08187fb65cbcc3768ced5
zomars <zomars@me.com>
2021-03-02 13:18:16 -0700

    Molecular Motion

--- rw_visual.py
+++ rw_visual.py
@@ -5,15 +5,14 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk(50000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-               edgecolor='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
 
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
