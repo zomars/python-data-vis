25f4a98bd0897c1d5d89caa0468651b77c0e04fd
zomars <zomars@me.com>
2021-03-02 11:25:47 -0700

    plot the first 5000 cubic numbers

--- plot_cubes.py
+++ plot_cubes.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-cubes = [1, 8, 27, 256, 3125]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, cubes, linewidth=3)
+ax.plot(x_values, y_values, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Cube Number", fontsize=24)
