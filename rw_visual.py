6da4e6e150886a2716e36e4c19dd672322602983
zomars <zomars@me.com>
2021-03-02 12:56:01 -0700

    Cleaning Up the Axes

--- rw_visual.py
+++ rw_visual.py
@@ -20,6 +20,10 @@ while True:
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
                s=100)
 
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
