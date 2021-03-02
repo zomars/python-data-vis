41c94f4b93dc79ce5927a4f42d3830ab4a8b395b
zomars <zomars@me.com>
2021-03-02 13:19:24 -0700

    Modified Random Walks

--- random_walk.py
+++ random_walk.py
@@ -19,8 +19,8 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go and how fat to go in that direction.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
+            x_direction = choice([1])
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
             x_step = x_direction * x_distance
 
             y_direction = choice([1, -1])
